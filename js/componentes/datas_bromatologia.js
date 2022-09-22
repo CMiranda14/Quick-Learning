1517295ebcf51bf1f4108f2054ff3572b45276c6
Cristopher <cdtum@scl.edu.gt>
2022-09-22 13:49:09 -0600

    Se agrega la parte inicial de carbohidratos

--- js/componentes/datas_bromatologia.js
+++ js/componentes/datas_bromatologia.js
@@ -7,3 +7,60 @@ export const matriz_video_proteinas =[
     {titulo:'¿SABÍAS ESTO?',	texto:'Las proteínas están formadas por una cadena de aminoácidos que ayudan al cuerpo a regenerarse y a tener la capacidad de defenderse ante sustancias extrañas. Tambien permiten que tengamos huesos fuertes.'},
     {titulo:'¿SABÍAS ESTO?',	texto:'De acuerdo con la Biblioteca Nacional de Medicina de los Estados Unidos, la ingesta recomendada de proteínas para los adultos saludables, y en una diera de 2,000 calorías, es de 100 gramos. Esto representa el 20% de un total de calorías diarias.'},
 ];
+
+
+/**DATAS PROTEINAS (SECCIÒN FRUTAS Y VEGETALES)**/
+
+export const Guayaba = [
+    {
+        nombre: 'Guayaba',
+        calorias: '0.051kcal-51cal',
+        libras: '1 lb',
+        proteina: '0,8g',
+        lipidos:'0,6g',
+        carbohidratos: '11,9g',
+        vitaminas: 'Vitamina A',
+        vitamina2: 'Vitamina C',
+        vitamina3: 'Vitamina E',
+        vitamina4: 'Vitamina D12',
+
+
+    }
+
+]
+
+export const Aceitunas = [
+    {
+        nombre: 'Aceitunas',
+        calorias: '0.126kcal-126cal',
+        libras: '1 lb',
+        proteina: '1,4g',
+        lipidos:'12,7g',
+        carbohidratos: '4,1g',
+        vitaminas: 'Vitamina A',
+        vitamina2: 'Vitamina E',
+
+
+    }
+
+]
+
+export const Platano = [
+    {
+        nombre: 'Plátano',
+        calorias: '0.085kcal-85cal',
+        libras: '1 lb',
+        proteina: '1,2g',
+        lipidos:'0,3g',
+        carbohidratos: '19,5g',
+        vitaminas: 'Vitamina B',
+        vitamina2: 'Vitamina C',
+        vitamina3: 'Vitamina E',
+        vitamina4: 'Vitamina K',
+
+
+    }
+
+]
+
+/**---------------------------------------------------**/
